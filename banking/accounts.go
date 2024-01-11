282e5f241598234d5704e6cac1eb1597ba506122
daeun <5ekdmsdldl@gmail.com>
2024-01-12 00:29:46 +0900

    dealing an exception

--- banking/accounts.go
+++ banking/accounts.go
@@ -1,10 +1,14 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("no money")
 
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
@@ -18,4 +22,12 @@ func (a *Account) Deposit(amount int) {
 
 func (a Account) Balance() int {
 	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil // == null
 }
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import accounts "GoLang/banking"
+import (
+	accounts "GoLang/banking"
+	"fmt"
+	"log"
+)
 
 func main() {
 	account := accounts.NewAccount("daeuna")
 	account.Deposit(10)
+	println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
 	println(account.Balance())
 }
